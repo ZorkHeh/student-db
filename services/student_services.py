6f75b22eb103efad66a1c7aaf02966ad046085fb
ZorkHeh <109086371+ZorkHeh@users.noreply.github.com>
2025-10-12 18:03:54 +0200

    search and delete funcs were added

--- services/student_services.py
+++ services/student_services.py
@@ -9,3 +9,12 @@ from storage.repository import (get_all_students,
 
 def services_get_all_students() -> list[Student]:
     return get_all_students()
+
+def services_search_student_by_last(last: str) -> list[Student] | list[None]:
+    return search_student_by_last(last)
+
+def services_search_student_by_pesel(pesel: str) -> Student | None:
+    return search_student_by_pesel(pesel)
+
+def services_delete_user_by_student_id(student_id: str) -> bool:
+    return delete_user_by_student_id(student_id)

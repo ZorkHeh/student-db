68013ca84784a926edc289cfcbb2a9f5093ceef6
ZorkHeh <109086371+ZorkHeh@users.noreply.github.com>
2025-10-12 18:16:53 +0200

    add student func was added

--- services/student_services.py
+++ services/student_services.py
@@ -6,6 +6,7 @@ from storage.repository import (get_all_students,
                                 search_student_by_pesel,
                                 delete_user_by_student_id)
 
+from validators.pesel import validate_pesel
 
 def services_get_all_students() -> list[Student]:
     return get_all_students()
@@ -18,3 +19,20 @@ def services_search_student_by_pesel(pesel: str) -> Student | None:
 
 def services_delete_user_by_student_id(student_id: str) -> bool:
     return delete_user_by_student_id(student_id)
+
+def services_add_student(first_name: str, last_name: str, address: str,
+                         student_id: str, pesel: str, gender: str) -> bool:
+    flag: bool = False
+    is_valid_pesel = validate_pesel(pesel)
+    if is_valid_pesel:
+        student = Student(first_name=first_name,
+                          last_name=last_name,
+                          address=address,
+                          student_id=student_id,
+                          pesel=pesel,
+                          gender=gender
+                          )
+
+        flag = add_student(student)
+
+    return flag
